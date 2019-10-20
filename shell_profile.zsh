f0495a58687d5f771f725c8a21bf96db1f9cbe39
Paul Viallard <paul.viallard@gmail.com>
2019-10-20 17:48:57 +0200

    feat: remove the previous folders

--- shell_profile.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-# Copyright © 2017 Paul Viallard <paul.viallard@gmail.com>
-# This work is free. You can redistribute it and/or modify it under the
-# terms of the Do What The Fuck You Want To Public License, Version 2,
-# as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
-
-source ~/.shell/config/config.zsh
-source ~/.shell/prompt/start.zsh
